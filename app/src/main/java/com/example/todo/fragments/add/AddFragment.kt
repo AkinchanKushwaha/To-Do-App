5b4c818f9aae2c629729c7fa66ed5e6137e049ef
AkinchanKushwaha <akinchandroger@gmail.com>
2021-06-10 01:54:06 +0530

    added menu for AddFragment.kt

--- app/src/main/java/com/example/todo/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/todo/fragments/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.example.todo.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.todo.R
 
 class AddFragment : Fragment() {
@@ -13,9 +11,19 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Set Menu
+        setHasOptionsMenu(true)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
+
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
+
 
 
 }
