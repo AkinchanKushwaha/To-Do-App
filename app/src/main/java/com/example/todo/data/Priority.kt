97dd2731b72cd372822bbe08369d36b912113a3b
AkinchanKushwaha <akinchandroger@gmail.com>
2021-06-11 01:12:48 +0530

    created Entity, Dao and Database

---
+++ app/src/main/java/com/example/todo/data/Priority.kt
@@ -0,0 +1,7 @@
+package com.example.todo.data
+
+enum class Priority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ app/src/main/java/com/example/todo/data/ToDoData.kt
@@ -0,0 +1,14 @@
+package com.example.todo.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todo_table")
+data class ToDoData(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var priority: Priority,
+    var description: String
+
+)
